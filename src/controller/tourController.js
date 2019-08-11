5048adacbc1ed7b7541cf3ee451ef758a99bd813
Ahmed Boghdady <boghdady@Ahmeds-iMac.local>
2019-08-11 21:55:59 +0200

    connect with mongodb

--- src/controller/tourController.js
+++ src/controller/tourController.js
@@ -1,82 +1,64 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-// Convert json to java script object
-const toursList = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`).toString()
-);
-
-// Param Middleware, value = the value of id param
-exports.checkID = (req, res, next, value) => {
-  console.log(`Tour id is : ${value}`);
-
-  if (value > toursList.length) {
-    return res.status(404).json({
+exports.getAllTours = async (req, res) => {
+  try {
+    const allTours = await Tour.find({});
+    res.status(200).json({
+      status: 'success',
+      results: allTours.length,
+      data: { tours: allTours }
+    });
+  } catch (err) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID'
+      message: err
     });
   }
-  next();
 };
-
-// another middleware for createTour Handler
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour }
+    });
+  } catch (err) {
+    res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price'
+      message: 'Invalid data sent'
     });
   }
-  next();
 };
-
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    result: toursList.length,
-    data: { tours: toursList }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: { tour: tour }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
-exports.createTour = (req, res) => {
-  const newId = toursList[toursList.length - 1].id + 1;
-  // Object.assign => combine two object in one object
-  const newTour = Object.assign({ id: newId }, req.body);
-  toursList.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(toursList),
-    err => {
-      if (err) return res.json({ error: err });
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
-};
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  // convert string to number
-  const id = req.params.id * 1;
-  const tour = toursList.find(element => element.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour
-    }
-  });
-};
-exports.updateTour = (req, res) => {
-  // success
-  return res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'put updated tour here ...'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: { tour }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 exports.deleteTour = (req, res) => {
   //success, 204 mean no content
--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 // Read the variables from config file and save them into node.js environment variables
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-//console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Connect to hosted database, to connect with local replace DB with (process.env.DATABASE_LOCAL)
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(conn => {
+    //console.log(conn.connections);
+    console.log('DB connected successfully');
+  });
+
 
 // Start Server
 const port = process.env.POPORT || 3000;
