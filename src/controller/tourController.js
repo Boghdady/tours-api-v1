8d1474a88d69dfc11b9a8deedd971d3d7470fdc6
index <index@MacBook-Pro-3.local>
2019-08-20 16:05:39 +0200

    Refactor code

--- src/controller/tourController.js
+++ src/controller/tourController.js
@@ -1,4 +1,6 @@
 const Tour = require('./../models/tourModel');
+const ApiFeatures = require('../utils/apiFeatures');
+
 
 // alias 1
 exports.aliasTopCheapestTours = (req, res, next) => {
@@ -18,69 +20,16 @@ exports.aliasTopRatingTours = (req, res, next) => {
   next();
 };
 
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
-    //************** 1) BUILD A QUERY *****************//
-    // create new object that takes all fields in query string
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    // delete excluded fields from queryObj if exists
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    console.log(queryObj);
-    // ======> A) Advanced Filtering using gte,gt,lte,lt
-    // Convert object to string
-    let queryStr = JSON.stringify(queryObj);
-    // replace any (gte,gt,lte,lt) with ($gte,$gt,$lte,$lt)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    // method 1) Filtering using query object (find return query)
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // method 2) Filtering using mongoose methods
-    // const allTours =  Tour.find({})
-    //   .where('duration').equals(5)
-    //   .where('difficulty').equals('easy');
-
-    // =========> B) Sorting (ask + ,desc -)
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy);
-    } else {
-      // default sort
-      query = query.sort('-createdAt');
-    }
-
-    /* ========> C) Fields limiting : allow clients to choose which field
-                    they wants to get back in the response.
-    */
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    /* =========> D) Pagination
-        page=2&limit=10 ==> 1-10 = page 1 , 11-20 = page 2 , 21-30 page 30
-     */
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query.skip(skip).limit(limit);
-
-    // validate if user enter page not exist
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     //************** 2) EXECUTE THE QUERY *****************//
-    const allTours = await query;
+    // const allTours = await query;
+
+    const apiFeatures = new ApiFeatures(Tour.find(), req.query).filter()
+      .sort().limitFields().paginate();
+    const allTours = await apiFeatures.mongooseQuery;
 
     //************** 3) SEND RESPONSE *****************//
     res.status(200).json({
@@ -94,6 +43,65 @@ exports.getAllTours = async (req, res) => {
       message: err
     });
   }
+
+
+  //************** 1) BUILD A QUERY *****************//
+  // // create new object that takes all fields in query string
+  // const queryObj = { ...req.query };
+  // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  // // delete excluded fields from queryObj if exists
+  // excludedFields.forEach(el => delete queryObj[el]);
+  //
+  // console.log(queryObj);
+  // // ======> A) Advanced Filtering using gte,gt,lte,lt
+  // // Convert object to string
+  // let queryStr = JSON.stringify(queryObj);
+  // // replace any (gte,gt,lte,lt) with ($gte,$gt,$lte,$lt)
+  // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+  // console.log(JSON.parse(queryStr));
+  //
+  // // method 1) Filtering using query object (find return query)
+  // let query = Tour.find(JSON.parse(queryStr));
+
+  // method 2) Filtering using mongoose methods
+  // const allTours =  Tour.find({})
+  //   .where('duration').equals(5)
+  //   .where('difficulty').equals('easy');
+
+  // // =========> B) Sorting (ask + ,desc -)
+  // if (req.query.sort) {
+  //   const sortBy = req.query.sort.split(',').join(' ');
+  //   console.log(sortBy);
+  //   query = query.sort(sortBy);
+  // } else {
+  //   // default sort
+  //   query = query.sort('-createdAt');
+  // }
+
+  /* ========> C) Fields limiting : allow clients to choose which field
+                  they wants to get back in the response.
+  */
+  // if (req.query.fields) {
+  //   const fields = req.query.fields.split(',').join(' ');
+  //   query = query.select(fields);
+  // } else {
+  //   query = query.select('-__v');
+  // }
+
+  /* =========> D) Pagination
+      page=2&limit=10 ==> 1-10 = page 1 , 11-20 = page 2 , 21-30 page 30
+   */
+  // const page = req.query.page * 1 || 1;
+  // const limit = req.query.limit * 1 || 100;
+  // const skip = (page - 1) * limit;
+  //
+  // query.skip(skip).limit(limit);
+  //
+  // // validate if user enter page not exist
+  // if (req.query.page) {
+  //   const numTours = await Tour.countDocuments();
+  //   if (skip >= numTours) throw new Error('This page does not exist');
+  // }
 };
 exports.createTour = async (req, res) => {
   try {
---
+++ src/utils/apiFeatures.js
@@ -0,0 +1,72 @@
+class ApiFeatures {
+  constructor(mongooseQuery, queryString) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryString = queryString;
+  }
+
+  // A) Advanced Filtering using gte,gt,lte,lt
+  filter() {
+    // create new object that takes all fields in query string
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // delete excluded fields from queryObj if exists
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    console.log(queryObj);
+    // ======> A) Advanced Filtering using gte,gt,lte,lt
+    // Convert object to string
+    let queryStr = JSON.stringify(queryObj);
+    // replace any (gte,gt,lte,lt) with ($gte,$gt,$lte,$lt)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    // method 1) Filtering using query object (find return query)
+    this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryStr));
+
+    // to return the object, to can do chaining
+    return this;
+  }
+
+  // B) Sorting (ask + ,desc -)
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      console.log(sortBy);
+      this.mongooseQuery = this.mongooseQuery.sort(sortBy);
+    } else {
+      // default sort
+      this.mongooseQuery = this.mongooseQuery.sort('-createdAt');
+    }
+    // to return the object, to can do chaining
+    return this;
+  }
+
+  /* ========> C) Fields limiting : allow clients to choose which field
+             they wants to get back in the response.
+  */
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.mongooseQuery = this.mongooseQuery.select(fields);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.select('-__v');
+    }
+    // to return the object, to can do chaining
+    return this;
+  }
+
+  /* =========> D) Pagination
+  page=2&limit=10 ==> 1-10 = page 1 , 11-20 = page 2 , 21-30 page 30
+  */
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
+    // to return the object, to can do chaining
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;
