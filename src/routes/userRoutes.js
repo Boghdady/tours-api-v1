2244613d211b7623a934dc8c4d1bc6162845b881
Ahmed Boghdady <boghdady@Ahmeds-iMac.local>
2019-09-04 03:59:09 +0200

    Adding Sign Up Route and Controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const userController = require('../controller/userController');
+const authController = require('../controller/authController');
 
 const router = express.Router();
 
-router
-  .route('/')
+router.post('/signup', authController.signUp);
+
+router.route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
-router
-  .route('/:id')
+
+router.route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
