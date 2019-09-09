18b98bc6d194a4174d9927601fab8ac8b2291a17
index <index@MacBook-Pro-3.local>
2019-09-09 22:05:28 +0200

    mac

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,13 +1,41 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // These fields are needed for signin
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+  // Create token for the new user
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 
   res.status(201).json({
     status: 'success',
+    token: token,
     data: {
       user: newUser
     }
   });
 });
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check of email and password exist in the body
+  if (!email || !password) {
+    return next(new AppError('Email and password are required'));
+  }
+  // 2) Check if user exist && password correct
+  // If everything ok, send token to client
+  const token = 'uweuy8';
+  res.status(200).json({
+    status: 'success',
+    token
+  });
+};
