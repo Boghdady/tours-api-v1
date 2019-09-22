97f6571ac1e1639d931e7ee45c2a95b20b055f79
index <index@MacBook-Pro-3.local>
2019-09-22 23:00:55 +0200

    Adding helmet package to adding some important security http headers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const AppError = require('./utils/appError');
 const globalErrorHandling = require('./controller/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,17 +10,35 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARE Working in all
+// 1) GLOBAL MIDDLEWARE WORKING IN ALL
+
+// helmet :  set important security http headers
+app.use(helmet());
+
+// Development Logging
 console.log(`NODE_ENV IS : ${process.env.NODE_ENV}`);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-// convert request body to json
+/*
+  Rate Limiter : it count the number of requests coming from one IP and when
+  there are too many request it block the request
+ */
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 100 request per one hour
+  message: 'Too many requests from this ip, please try again in an hour!'
+});
+app.use('/api', limiter); // This limiter will affect in routes that start with api
+
+// Body parser, reading data from body into req.body, options( limit => body accept data in size 10kb)
+// app.use(express.json({ limit: '10kb' }));
 app.use(express.json());
-// serve static file
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// Test the sequence of middleware
+// Test the sequence of middleware (only test middleware you can delete it)
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log('Header', req.headers);
