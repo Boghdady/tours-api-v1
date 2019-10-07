f8d297434b2d662869988cfb9521786c5ce827cb
index <index@MacBook-Pro-3.local>
2019-10-07 14:47:09 +0200

    Adding Authentication and autherization in routes

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -6,20 +6,23 @@ const authController = require('./../controller/authController');
 // mergeParams : allow us to access the parameters on other routers
 const router = express.Router({ mergeParams: true });
 
+// That mean => protect all the routes that coming after this middleware
+router.use(authController.protect);
+
 // this route can accept these all requests
 // POST /tour/1276dh2sd/reviews ==> Coming from nested rout in touRoute
 // GET /tour/1276dh2sd/reviews  ==> Coming from nested rout in touRoute
 // Post /reviews
 // GET /reviews
 router.route('/')
-  .get(authController.protect, reviewController.createFilterObjectForNestedRoute, reviewController.getAllReviews)
-  .post(authController.protect, authController.restrictTo('user'),
+  .get(reviewController.createFilterObjectForNestedRoute, reviewController.getAllReviews)
+  .post(authController.restrictTo('user'),
     reviewController.setUserIdAndTourId, reviewController.createReview);
 
 router.route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
+  .delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview);
 
 
 module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,16 +4,21 @@ const authController = require('../controller/authController');
 
 const router = express.Router();
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
-
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 
+// That mean => protect all the routes that coming after this middleware
+router.use(authController.protect);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+
+// That mean => protect and perform this actions by the admin to all the routes that coming after this middleware
+router.use(authController.restrictTo('admin'));
 
 router.route('/')
   .get(userController.getAllUsers)
